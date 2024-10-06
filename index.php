8cc9d005005152edb0f4b213d3719e3fdcd0b63e
Pranav Devkar <devpranavdevkar@gmail.com>
2024-10-07 00:18:35 +0530

    Varibales

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+echo "Codse Step By Step <br/>";
+echo 1+3+1+1+1+9+9+7;
+echo "<br/>" ;
+echo("Echo ues as a function")
+?>
---
+++ variable.php
@@ -0,0 +1,35 @@
+<?php
+$name = "Pranav"; //String
+$num1 = 13; //Int
+$num2 = 19.97; //Decimal
+$num3 = 11;
+$num4 = 1997;
+$var1= $var2= 13111997;
+
+echo $name;
+echo "<br/>";
+
+echo $num1;
+echo "<br/>";
+
+echo $num2;
+echo "<br/>";
+
+echo ($var1+$var2);
+echo "<br/>";
+
+/*
+RULES FOR VARIABLE DECLARATION IN PHP;
+1.A Variables start with $ sign;
+2.A Variable can not start with number and special characters;
+3.A variable name start with alphabets or underscore;
+4.$name $Name $NAME these are three diffrent variables;
+5.A variable name can only contain alphanumric and underscore;
+6.Variable Syntax 
+  $name= "value of variable";
+  $name= "Pranav"
+*/
+
+
+
+?>
